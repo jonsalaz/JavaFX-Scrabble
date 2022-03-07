1d78528d718283e1cbe3ca070efc43f4fbebf485
Jonathan Salazar <jonsalaz23@unm.edu>
2022-03-07 16:21:51 -0700

    Removed code to begin readjusting process.

--- src/Board.java
+++ src/Board.java
@@ -5,25 +5,8 @@ import java.util.Scanner;
 public class Board {
     private Tile[][] board;
 
-    public Board(File file) throws FileNotFoundException {
+    public Board(int dim, String[] rows) {
         //TODO: Readjust parsing in order to take in multiple configs in main
-        Scanner scanner = new Scanner(file);
-        int dim = scanner.nextInt();
         board = new Tile[dim][dim];
-
-        for(int i = 0; i < dim; i++) {
-            String line = scanner.nextLine();
-            String[] row = line.split("\\r+");
-
-            int column = 0;
-            for (String tile: row) {
-                if(tile.length() == 2) {
-                    board[i][column] = new Tile(tile.charAt(0), tile.charAt(1));
-                }
-                else {
-                    board[i][column] = new Tile(tile.charAt(0));
-                }
-            }
-        }
     }
 }

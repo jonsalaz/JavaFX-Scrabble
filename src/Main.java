d6abadcf423fcea835ebe715f59ffbee17cc6050
Jonathan Salazar <jonsalaz23@unm.edu>
2022-03-07 16:20:55 -0700

    Parse config file through completion.

--- src/Main.java
+++ src/Main.java
@@ -5,25 +5,52 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Trie trie;
-        Board board;
+
+        File config;
+        Scanner fileScanner;
+
         Scanner scanner = new Scanner(System.in);
-        try {
-            trie = new Trie("testing/twl06.txt");
-            System.out.println(trie);
-        }
-        catch (FileNotFoundException e) {
-            System.out.println("File not found");
-        }
-        catch (Exception e) {
-            e.printStackTrace();
+
+        while(true) {
+            try {
+                trie = new Trie("testing/twl06.txt");
+                break;
+            } catch (FileNotFoundException e) {
+                System.out.println("Dictionary config file not found");
+            }
         }
 
-        try{
-            System.out.println("Please enter the name of your board configuration file.");
-            File file = new File(scanner.next());
-            board = new Board(file);
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found");
+        while(true) {
+            System.out.println("Please enter your tray config files");
+            try {
+                config = new File(scanner.nextLine());
+                fileScanner = new Scanner(config);
+                break;
+            } catch (Exception e) {
+                System.out.println("File not found");
+            }
+        }
+
+        while(fileScanner.hasNextLine()) {
+            // Necessary objects to run solver.
+            Board board;
+            Tray tray;
+            int dim;
+
+            // Parse file for dimensions.
+            dim = fileScanner.nextInt();
+
+            // Parse file for row values.
+            String[] rows = new String[dim];
+            for(int i = 0; i < dim; i++) {
+                rows[i] = fileScanner.nextLine();
+            }
+
+            // Create Necessary Objects for solver.
+            board = new Board(dim, rows);
+            tray = new Tray(scanner.nextLine());
+
+            // TODO: Solve
         }
     }
 }

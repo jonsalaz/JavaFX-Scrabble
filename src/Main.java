8bde2a6965933e1f0714f849cbc44ee9e1723a56
Jonathan Salazar <jonsalaz23@unm.edu>
2022-03-07 15:41:09 -0700

    Parse board config file

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Trie trie;
+        Board board;
+        Scanner scanner = new Scanner(System.in);
         try {
-            Trie trie = new Trie("testing/twl06.txt");
+            trie = new Trie("testing/twl06.txt");
             System.out.println(trie);
         }
         catch (FileNotFoundException e) {
@@ -11,6 +16,14 @@ public class Main {
         }
         catch (Exception e) {
             e.printStackTrace();
+        }
+
+        try{
+            System.out.println("Please enter the name of your board configuration file.");
+            File file = new File(scanner.next());
+            board = new Board(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
         }
     }
 }

10fb5b232079728bd102e19cc49ce80cc42fec71
Jonathan Salazar <jonsalaz23@gmail.com>
2022-03-07 19:08:57 -0700

    @Override toString()

--- src/Tray.java
+++ src/Tray.java
@@ -1,17 +1,27 @@
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class Tray {
-    private ArrayList<Tile> tray;
+    private ArrayList<Character> tray;
 
     public Tray(String letters) {
+        //TODO: Make this a playable letter not a tile space for game tiles...
         this.tray = new ArrayList<>();
         for(int i = 0; i < letters.length(); i++) {
-            tray.add(new Tile(letters.charAt(i)));
+            tray.add(letters.charAt(i));
         }
     }
 
-    public ArrayList<Tile> getTray() {
+    public ArrayList<Character> getTray() {
         return tray;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder temp = new StringBuilder("Tray: ");
+        for (Character letter: tray) {
+            temp.append(letter);
+        }
+
+        return temp.toString();
     }
 }

b761ab9b0f849bc3c2c738c1f9cf6f5a02dfc221
Jonathan Salazar <jonsalaz23@gmail.com>
2022-03-07 20:42:02 -0700

    leftPart of scrabble solver completed

--- src/Solver.java
+++ src/Solver.java
@@ -1,6 +1,6 @@
 public class Solver {
-    private Tray tray;
     private Board board;
+    private Tray tray;
     private Trie trie;
 
     public Solver(Board board, Tray tray, Trie trie) {
@@ -11,5 +11,23 @@ public class Solver {
 
     public void solve() {
         // TODO: solve
+    }
+
+    private void leftPart(String partialWord, TrieNode N, int limit, Square anchorSquare){
+        extendRight(partialWord, N, anchorSquare);
+        if(limit > 0) {
+            for(int i = 0; i < N.getChildren().length; i++) {
+                if ((N.getChildren()[i] == null) && tray.contains((char) (i+97))) {
+                    Tile l = tray.get((char) (i+97));
+                    TrieNode newN = N.getChildren()[i];
+                    leftPart(partialWord + l.getLetter(), newN, limit - 1, anchorSquare);
+                    tray.add(l);
+                }
+            }
+        }
+    }
+
+    private void extendRight(String partialWord, TrieNode N, Square square){
+
     }
 }

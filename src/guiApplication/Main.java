04186151bd3946b90d61add2e364fe58df38e68b
Jonathan Salazar <jonsalaz23@unm.edu>
2022-03-25 08:31:54 -0600

    Moved fxml file to resources.

--- src/guiApplication/Main.java
+++ src/guiApplication/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("scrabble.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/scrabble.fxml"));
         primaryStage.setTitle("Scrabble!");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
